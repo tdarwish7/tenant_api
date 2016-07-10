1815614fec755c61f38767de6e91aa184036a286
Teacher <Teacher>
2016-07-10 15:51:11 -0400

    added constructor model for tenant listings

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var bodyParser = require('body-parser');
 var lowdb = require('lowdb');
 var uuid = require('uuid');
 
-
+//import my model
+var Listing = require('./models/listing.js')
 var port = process.env.PORT || 8080;
 var db = lowdb('db.json');
 
@@ -29,22 +30,12 @@ server.get('/listings/:id', function (request, response){
 });
 
 server.post('/listings', function (request, response){
-  var listing ={
-    id: uuid.v4(),
-    location: request.body.location,
-    houseOrApt: request.body.houseOrApt,
-    numberOfRooms: request.body.numberOfRooms,
-    numberOfBathrooms: request.body.numberOfBathrooms,
-    numberOfRoommates: request.body.numberOfRoommates,
-    typeOfLease: request.body.typeOfLease,
-    arePetsAllowed: request.body.arePetsAllowed
-  };
-
+  var listing = new Listing(request.body.location, request.body.houseOrApt, request.body.numberOfRooms, request.body.numberOfBathrooms, request.body.numberOfRoommates, request.body.typeOfLease, request.body.arePetsAllowed)
   var result = db.get('listings')
-                .push(listing)
-                .last()
-                .value();
-  response.send(result);
+              .push(listing)
+              .last()
+              .value();
+    response.send(result);
 });
 
 server.put('/listings/:id', function (request, response){
